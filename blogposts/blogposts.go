fd3d67044d74c768c172c03ee553f1cc6e7f02e5
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-08-30 16:51:13 +0500

    feat: new Post field Body

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -2,7 +2,9 @@ package blogposts
 
 import (
 	"bufio"
+	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"io/fs"
 	"strings"
@@ -12,6 +14,7 @@ type Post struct {
 	Title       string
 	Description string
 	Tags        []string
+	Body        string
 }
 
 type StubFailingFS struct {
@@ -60,9 +63,22 @@ func newPost(postFile io.Reader) (Post, error) {
 		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
 
+	title := readMetaLine(titleSeparator)
+	description := readMetaLine(descriptionSeparator)
+	tags := strings.Split(readMetaLine(tagsSeparator), ", ")
+
+	scanner.Scan() //ignore line
+
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	body := strings.TrimSuffix(buf.String(), "\n")
+
 	return Post{
-		Title:       readMetaLine(titleSeparator),
-		Description: readMetaLine(descriptionSeparator),
-		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Title:       title,
+		Description: description,
+		Tags:        tags,
+		Body:        body,
 	}, nil
 }
