2cb155083ae537b3c8686f62dee98dada37895b4
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-09-01 01:15:44 +0500

    perf: improved render benchmark speed

--- blogrenderer/renderer.go
+++ blogrenderer/renderer.go
@@ -11,6 +11,10 @@ type Post struct {
 	Tags                     []string
 }
 
+type PostRenderer struct {
+	templ *template.Template
+}
+
 var (
 	//go:embed "templates/*"
 	postTemplates embed.FS
@@ -22,7 +26,25 @@ func Render(w io.Writer, p Post) error {
 		return err
 	}
 
-	if err := templ.ExecuteTemplate(w,"blog.html", p); err != nil {
+	if err := templ.ExecuteTemplate(w, "blog.html", p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.html")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+
+	if err := r.templ.ExecuteTemplate(w, "blog.html", p); err != nil {
 		return err
 	}
 
