8e29b9f2d8d8ae689bbd8c5c25ae46d13ad942cd
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-09-01 02:47:52 +0500

    feat: can render markdown body

--- blogrenderer/renderer.go
+++ blogrenderer/renderer.go
@@ -5,6 +5,9 @@ import (
 	"html/template"
 	"io"
 	"strings"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
 )
 
 type Post struct {
@@ -18,7 +21,8 @@ type PostViewModel struct {
 }
 
 type PostRenderer struct {
-	templ *template.Template
+	templ    *template.Template
+	mdParser *parser.Parser
 }
 
 var (
@@ -49,13 +53,27 @@ func NewPostRenderer() (*PostRenderer, error) {
 		return nil, err
 	}
 
-	return &PostRenderer{templ: templ}, nil
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	parser := parser.NewWithExtensions(extensions)
+
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
 }
 
 func (r *PostRenderer) Render(w io.Writer, p Post) error {
-	return r.templ.ExecuteTemplate(w, "blog.html", p)
+	return r.templ.ExecuteTemplate(w, "blog.html", newPostVM(p, r))
 }
 
 func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
 	return r.templ.ExecuteTemplate(w, "index.html", posts)
+}
+
+type postViewModel struct {
+	Post
+	HTMLBody template.HTML
+}
+
+func newPostVM(p Post, r *PostRenderer) postViewModel {
+	vm := postViewModel{Post: p}
+	vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	return vm
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module learngowithtests/hello
 
 go 1.23
 
-require github.com/approvals/go-approval-tests v0.0.0-20240417152556-434b9105e958
+require (
+	github.com/approvals/go-approval-tests v0.0.0-20240417152556-434b9105e958
+	github.com/gomarkdown/markdown v0.0.0-20240730141124-034f12af3bf6
+)
