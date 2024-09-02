ab188c05c97bb9535798ddd33fa6172eab9bb527
Artyom Arsalanov <artyom.arsalanov@gmail.com>
2024-09-02 12:50:13 +0500

    feat: new func and test Reduce

--- array_slice/sum.go
+++ array_slice/sum.go
@@ -1,11 +1,18 @@
 package main
 
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	return 0.0
+}
+
 func Sum(numbers []int) int {
-	sum := 0
-	for _, number := range numbers {
-		sum += number
-	}
-	return sum
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
 }
 
 func SumAll(numbersToSum ...[]int) []int {
@@ -18,15 +25,22 @@ func SumAll(numbersToSum ...[]int) []int {
 }
 
 func SumAllTails(numbersToSum ...[]int) []int {
-	var sums []int
-	for _, numbers := range numbersToSum {
-		if len(numbers) == 0 {
-			sums = append(sums, 0)
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
 		} else {
-			tail := numbers[1:]
-			sums = append(sums, Sum(tail))
+			tail := x[1:]
+			return append(acc, Sum(tail))
 		}
 	}
 
-	return sums
+	return Reduce(numbersToSum, sumTail, []int{})
+}
+
+func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = f(result, x)
+	}
+	return result
 }
--- array_slice/sum_test.go
+++ array_slice/sum_test.go
@@ -1,22 +1,12 @@
 package main
 
 import (
+	"learngowithtests/hello/generics"
 	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
-	// t.Run("collection of 5 numbers", func(t *testing.T) {
-	// 	numbers := []int{1, 2, 3, 4, 5}
-
-	// 	got := Sum(numbers)
-	// 	want := 15
-
-	// 	if got != want {
-	// 		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	// 	}
-	// })
-
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 
@@ -57,4 +47,41 @@ func TestSumAllTails(t *testing.T) {
 		want := []int{0, 9}
 		checkSums(t, got, want)
 	})
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		generics.AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		generics.AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func TestBadBank(t *testing.T) {
+	transactions := []Transaction{
+		{
+			From: "Chris",
+			To:   "Riya",
+			Sum:  100,
+		},
+		{
+			From: "Adil",
+			To:   "Chris",
+			Sum:  25,
+		},
+	}
+
+	generics.AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
+	generics.AssertEqual(t, BalanceFor(transactions, "Chris"), -75)
+	generics.AssertEqual(t, BalanceFor(transactions, "Adil"), -25)
 }
